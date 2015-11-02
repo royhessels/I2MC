46db7d6679b4f334b494f0d0c734c870ec5c50ca
Diederick Niehorster <dcnieho@gmail.com>
2015-11-02 21:18:52 +0100

    more cleanup

--- functions/importTobiiTX300.m
+++ functions/importTobiiTX300.m
@@ -2,34 +2,15 @@ function [timestamp,lx,ly,rx,ry] = importTobiiTX300(file,nskip,res,missingx,miss
 % Imports data from Tobii TX300 as return by Tobii SDK (programmed by RH)
 % res = [xres yres]
 
-if 0
-    fid = fopen(file);
-    
-    
-    for p=1:nskip
-        fgetl(fid);
-    end
-    
-    
-    dummy   = textscan(fid,'%f%d%d%d%f%d%d%f%f%f%d%d%f%d%d%d%f%d%d%d%f%f%d%d%d%d%d%f','delimiter','\t');
-    fclose(fid);
-    
-    timestamp     = double(dummy{:,28});
-    lx     = double(dummy{:,8}) * res(1);
-    ly     = double(dummy{:,9}) * res(2);
-    rx     = double(dummy{:,21}) * res(1);
-    ry     = double(dummy{:,22}) * res(2);
-else
-    % faster using readintfile (uitgebreid getest, er is geen snellere
-    % manier)
-    dat = readintfile(file,nskip,28);
-    
-    timestamp   = dat(:,28);
-    lx          = dat(:,8 ) * res(1);
-    ly          = dat(:,9 ) * res(2);
-    rx          = dat(:,21) * res(1);
-    ry          = dat(:,22) * res(2);
-end
+% faster using readintfile (uitgebreid getest, er is geen snellere
+% manier)
+dat = readintfile(file,nskip,28);
+
+timestamp   = dat(:,28);
+lx          = dat(:,8 ) * res(1);
+ly          = dat(:,9 ) * res(2);
+rx          = dat(:,21) * res(1);
+ry          = dat(:,22) * res(2);
 
 % sometimes we have weird peaks where one sample is (very) far outside the
 % monitor. Here, count as missing any data that is more than one monitor
