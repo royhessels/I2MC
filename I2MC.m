6e1d377a2048f2a3e7dca85605a933b9db6c072f
Diederick C. Niehorster <dcnieho@gmail.com>
2016-06-24 10:06:57 +0200

    release v0.9

---
+++ I2MC.m
@@ -0,0 +1,157 @@
+%% FIXATION DETECTION USING THE IDENTIFICATION BY 2-MEANS CLUSTERING (I2MC) ALGORITHM
+% Description:
+% The I2MC algorithm was designed to accomplish fixation detection in data
+% across a wide range of noise levels and when periods of data loss may
+% occur.
+
+% Cite as:
+% Hessels, R.S., Niehorster, D.C., Kemner, C., & Hooge, I.T.C., (2016).
+% Noise-robust fixation detection in eye-movement data - Identification by 
+% 2-means clustering (I2MC). Submitted.
+
+% For more information, questions, or to check whether we have updated to a
+% better version, e-mail: royhessels@gmail.com / dcnieho@gmail.com. I2MC is
+% available from www.github.com/royhessels/I2MC
+
+% Most parts of the I2MC algorithm are licensed under the Creative Commons
+% Attribution 4.0 (CC BY 4.0) license. Some functions are under MIT 
+% license, and some may be under other licenses.
+
+% Quick start guide for adopting this script for your own data:
+% 1) Build an import function specific for your data (see importTobiiTX300
+% for an example). 
+
+% 2) Change line 106 to use your new import function. The format should be:
+%
+% data.time for the timestamp
+% data.left.X & data.left.Y for left gaze coordinates
+% data.right.X & data.right.Y for right gaze coordinates
+% data.average.X & data.average.Y for average gaze coordinates
+% 
+% You may provide coordinates from both eyes, only the left, only the
+% right, or only the average.
+% Gaze coordinates should be in pixels, timestamps should be in milliseconds
+
+% 3) Adjust the variables in the "necessary variables" section to match your
+%    data
+% 4) Run the algorithm
+
+% Requirements: Signal Processing Toolbox for downsampling. If not
+% available you may set opt.downsample to []. This may, however, degrade
+% performance of the algorithm.
+
+% Tested on MATLAB R2012a, R2014b & R2016a
+
+%% INITIALIZE
+clear variables; clear mex; close all; fclose('all'); clc;
+dbstop if error;
+commandwindow;
+
+%% NECESSARY VARIABLES
+
+% General variables for eye-tracking data
+opt.xres                        = 1920; % maximum value of horizontal resolution in pixels
+opt.yres                        = 1080; % maximum value of vertical resolution in pixels
+opt.missingx                    = -opt.xres; % missing value for horizontal position in eye-tracking data (example data uses -xres). used throughout functions as signal for data loss
+opt.missingy                    = -opt.yres; % missing value for vertical position in eye-tracking data (example data uses -yres). used throughout functions as signal for data loss
+opt.freq                        = 300; % sampling frequency of data (check that this value matches with values actually obtained from measurement!)
+
+% Variables for the calculation of visual angle
+% These values are used to calculate noise measures (RMS and BCEA) of
+% fixations. The may be left as is, but don't use the noise measures then.
+opt.scrSz                       = [50.9174 28.6411]; % screen size in cm
+opt.disttoscreen                = 65; % distance to screen in cm.
+
+% Folders
+% Data folder should be structured by one folder for each participant with
+% the eye-tracking data in textfiles in each folder.
+folders.data                    = 'example data'; % folder in which data is stored (each folder in folders.data is considered 1 subject)
+folders.output                  = 'output'; % folder for output (will use structure in folders.data for saving output)
+
+% Plot results
+do.plots                        = 1; % if set to 1, plot of fixation detection for each trial will be saved as png-file in output folder.
+% the figures works best for short trials (up to around 20 seconds)
+
+%% OPTIONAL VARIABLES
+% The settings below may be used to adopt the default settings of the
+% algorithm. Do this only if you know what you're doing. Uncomment the
+% settings below and run the algorithm.
+
+% % STEFFEN INTERPOLATION
+% opt.windowtimeInterp            = 0.1;  % max duration (s) of missing values for interpolation to occur
+% opt.edgeSampInterp              = 2;    % amount of data (number of samples) at edges needed for interpolation
+% opt.maxdisp                     = opt.xres*0.2*sqrt(2); % maximum displacement during missing for interpolation to be possible
+% 
+% % K-MEANS CLUSTERING
+% opt.windowtime                  = 0.2; % time window (s) over which to calculate 2-means clustering (choose value so that max. 1 saccade can occur)
+% opt.steptime                    = 0.02;% time window shift (s) for each iteration. Use zero for sample by sample processing
+% opt.maxerrors                   = 100; % maximum number of errors allowed in k-means clustering procedure before proceeding to next file
+% opt.downsamples                 = [2 5 10];
+% 
+% % FIXATION DETERMINATION
+% opt.cutoffstd                   = 2; % number of standard deviations above mean k-means weights will be used as fixation cutoff
+% opt.maxMergeDist                = 30; % maximum Euclidean distance in pixels between fixations for merging
+% opt.maxMergeTime                = 30; % maximum time in ms between fixations for merging
+% opt.minFixDur                   = 40; % minimum fixation duration after merging, fixations with shorter duration are removed from output
+
+%% SET-UP FOLDERS
+
+folders.func                = 'functions'; % folder for functions, will add to matlab path
+addpath(genpath(folders.func));
+
+%% START ALGORITHM
+
+% create textfile and open for writing fixations
+fid = fopen(fullfile(folders.output,'allfixations.txt'),'w');
+fprintf(fid,'FixStart\tFixEnd\tFixDur\tXPos\tYPos\tFlankedByDataLoss\tFraction Interpolated\tWeightCutoff\tRMSxy\tBCEA\tFixRangeX\tFixRangeY\tParticipant\tTrial\n');
+
+% go through all folders in 'data' (one folder is assumed to be one
+% subject)
+[fold,nfold] = FolderFromFolder(folders.data);
+
+for e = 1:nfold
+    
+    % make output folder
+    mkdir(folders.output,fold(e).name);
+    
+    % go through all files in fold, if fold is empty continue to next
+    % folder
+    [file,nfile] = FileFromFolder(fullfile(folders.data,fold(e).name),'silent','txt');
+    if ~nfile
+        disp('folder is empty, continuing to next folder');
+        continue
+    end
+    
+    for f = 1:nfile
+       
+        %% IMPORT DATA
+        fprintf('Importing and processing %s/%s \n',fold(e).name,file(f).name)
+        
+        [data.time,data.left.X,data.left.Y,data.right.X,data.right.Y] = importTobiiTX300(fullfile(folders.data,fold(e).name,file(f).name),1,[opt.xres opt.yres],opt.missingx,opt.missingy);
+        
+        % check whether we have data, if not, continue to next file
+        if isempty(data.time)
+            fprintf('Empty file encountered, continuing to next file \n');
+            continue
+        end
+        
+        %% RUN FIXATION DETECTION
+        fix          = I2MCfunc(data,opt);
+        
+        %% PLOT RESULTS
+        
+        if do.plots
+            % pre-allocate name for saving file
+            savefile = fullfile(folders.output, fold(e).name, file(f).name(1:end-4));
+            plotResults(data,fix,savefile,[opt.xres opt.yres]);
+        end
+        
+        for g=1:numel(fix.start)
+            fprintf(fid,'%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%s\t%s\n',[fix.startT(g) fix.endT(g) fix.dur(g) fix.xpos(g) fix.ypos(g) fix.flankdataloss(g) fix.fracinterped(g) fix.cutoff, fix.RMSxy(g), fix.BCEA(g), fix.fixRangeX(g), fix.fixRangeY(g)],fold(e).name,file(f).name(1:end-4));
+        end
+    end
+end
+
+%% CLEAN UP
+
+fclose(fid);
---
+++ functions/I2MC/I2MCfunc.m
@@ -0,0 +1,199 @@
+function [fix,data,par] = I2MCfunc(data,varargin)
+% Hessels, R.S., Niehorster, D.C., Kemner, C., & Hooge, I.T.C., (2016).
+% Noise-robust fixation detection in eye-movement data - Identification by 
+% 2-means clustering (I2MC). Submitted.
+
+%% deal with inputs
+% get inputs the user specified and throw them in the parser
+if isstruct(varargin{1})
+    % convert to key-value pairs
+    assert(isscalar(varargin),'only one input for options is expected if options are given as a struct')
+    varargin = [reshape([fieldnames(varargin{1}) struct2cell(varargin{1})].',1,[]) varargin(2:end)];
+end
+
+% set defaults
+% required parameters:
+par.xres            = [];
+par.yres            = [];
+par.freq            = [];
+par.missingx        = [];
+par.missingy        = [];
+par.scrSz           = [];
+par.disttoscreen    = [];
+% parameters with defaults:
+% CUBIC SPLINE INTERPOLATION
+par.windowtimeInterp = .1;      % max duration (s) of missing values for interpolation to occur
+par.edgeSampInterp  = 2;        % amount of data (number of samples) at edges needed for interpolation
+par.maxdisp         = [];       % (default value set below if needed) maximum displacement during missing for interpolation to be possible
+% K-MEANS CLUSTERING
+par.windowtime      = .2;       % time window (s) over which to calculate 2-means clustering (choose value so that max. 1 saccade can occur)
+par.steptime        = .02;      % time window shift (s) for each iteration. Use zero for sample by sample processing
+par.downsamples     = [2 5 10]; % downsample levels (can be empty)
+par.chebyOrder      = 8;        % order of cheby1 Chebyshev downsampling filter, default is normally ok, as long as there are 25 or more samples in the window (you may have less if your data is of low sampling rate or your window is small
+par.maxerrors       = 100;      % maximum number of errors allowed in k-means clustering procedure before proceeding to next file
+% FIXATION DETERMINATION
+par.cutoffstd       = 2;        % number of standard deviations above mean k-means weights will be used as fixation cutoff
+par.maxMergeDist    = 30;       % maximum Euclidean distance in pixels between fixations for merging
+par.maxMergeTime    = 30;       % maximum time in ms between fixations for merging
+par.minFixDur       = 40;       % minimum fixation duration (ms) after merging, fixations with shorter duration are removed from output
+
+
+% loop over input
+checkNumeric = @(x,k) assert(isnumeric(x),'The value of ''%s'' is invalid. Expected input to be one of these types:\n\ndouble, single, uint8, uint16, uint32, uint64, int8, int16, int32, int64\n\nInstead its type was %s.',k,class(x));
+checkScalar  = @(x,k) assert(isscalar(x),'The value of ''%s'' is invalid. Expected input to be a scalar.',k);
+checkNumel2  = @(x,k) assert(numel(x)==2,'The value of ''%s'' is invalid. Expected input to be an array with number of elements equal to 2.',k);
+checkInt  = @(x,k) assert(isinteger(x),'The value of ''%s'' is invalid. Expected input to be integer-valued.',k);
+for p=1:2:length(varargin)
+    key = varargin{p};
+    if p+1>length(varargin)
+        error('No value was given for ''%s''. Name-value pair arguments require a name followed by a value.',key);
+    end
+    value = varargin{p+1};
+    switch key
+        case {'xres','yres','freq','missingx','missingy','disttoscreen','windowtimeInterp','maxdisp','windowtime','steptime','cutoffstd','maxMergeDist','maxMergeTime','minFixDur'}
+            checkNumeric(value,key);
+            checkScalar(value,key);
+            par.(key) = value;
+        case {'chebyOrder','maxerrors','edgeSampInterp'}
+            checkInt(value,key);
+            checkScalar(value,key);
+            par.(key) = value;
+        case 'scrSz'
+            checkNumeric(value,key);
+            checkNumel2(value,key);
+            par.(key) = value;
+        case 'downsamples'
+            checkInt(value,key);
+            par.(key) = value;
+        otherwise
+            if ~ischar(key)
+                error('Expected a string for the parameter name at position %d, instead the input type was ''%s''.',class(key));
+            else
+                error('Key "%s" not recognized',key);
+            end
+    end
+end
+
+% deal with required options
+% if empty, user did not specify these
+checkFun = @(opt,str) assert(~isempty(par.(opt)),'I2MCfunc: %s must be specified using the ''%s'' option',str,opt);
+checkFun('xres', 'horizontal screen resolution')
+checkFun('yres',   'vertical screen resolution')
+checkFun('freq', 'tracker sampling rate')
+checkFun('missingx', 'value indicating data loss for horizontal position')
+checkFun('missingy', 'value indicating data loss for vertical position')
+checkFun('scrSz', 'screen size ([x y]) in cm')
+checkFun('disttoscreen', 'distance to screen in cm')
+% process parameters with defaults
+if isempty(par.maxdisp)
+    par.maxdisp               = par.xres*0.2*sqrt(2); % maximum displacement during missing for interpolation to be possible
+end
+
+% setup visual angle conversion
+pixpercm                    = mean([par.xres par.yres]./par.scrSz(:).');
+rad2deg                     = @(x) x/pi*180;
+degpercm                    = 2*rad2deg(atan(1/(2*par.disttoscreen)));
+pixperdeg                   = pixpercm/degpercm;
+
+%% START ALGORITHM
+
+%% PREPARE INPUT DATA
+% deal with monocular data, or create average over two eyes
+if isfield(data,'left') && ~isfield(data,'right')
+    xpos = data.left.X;
+    ypos = data.left.Y;
+    missing = isnan(data.left.X) | data.left.X==par.missingx | isnan(data.left.Y) | data.left.Y==par.missingy;
+    data.left.missing = missing;
+    q2Eyes = false;
+elseif isfield(data,'right') && ~isfield(data,'left')
+    xpos = data.right.X;
+    ypos = data.right.Y;
+    missing = isnan(data.right.X) | data.right.X==par.missingx | isnan(data.right.Y) | data.right.Y==par.missingy;
+    data.right.missing = missing;
+    q2Eyes = false;
+elseif isfield(data,'average')
+    xpos = data.average.X;
+    ypos = data.average.Y;
+    missing = isnan(data.average.X) | data.average.X==par.missingx | isnan(data.average.Y) | data.average.Y==par.missingy;
+    data.average.missing = missing;
+    q2Eyes = isfield(data,'right') && isfield(data,'left');
+    if q2Eyes
+        % we have left and right and average already provided, but we need
+        % to get missing in the individual eye signals
+        [llmiss, rrmiss] = getMissing(data.left.X,data.right.X,par.missingx,data.left.Y,data.right.Y,par.missingy);
+        data.left.missing  = llmiss;
+        data.right.missing = rrmiss;
+    end
+else % we have left and right, average them
+    [data.average.X, data.average.Y, missing, llmiss, rrmiss] = averageEyes(data.left.X,data.right.X,par.missingx,data.left.Y,data.right.Y,par.missingy);
+    xpos = data.average.X;
+    ypos = data.average.Y;
+    data.average.missing = missing;
+    data.left.missing    = llmiss;
+    data.right.missing   = rrmiss;
+    q2Eyes = true;
+end
+
+%% INTERPOLATION
+
+% get interpolation windows for average and individual eye signals
+fprintf('Searching for valid interpolation windows\n');
+interpwins   = findInterpWins(xpos, ypos, missing,par.windowtimeInterp,par.edgeSampInterp,par.freq,par.maxdisp);
+if q2Eyes
+    llinterpwins = findInterpWins(data.left.X  ,data.left.Y  ,llmiss ,par.windowtimeInterp,par.edgeSampInterp,par.freq,par.maxdisp);
+    rrinterpwins = findInterpWins(data.right.X ,data.right.Y ,rrmiss ,par.windowtimeInterp,par.edgeSampInterp,par.freq,par.maxdisp);
+end
+
+% Use Steffen interpolation and replace values
+fprintf('Replace interpolation windows with Steffen interpolation\n');
+[xpos,ypos,missingn]= windowedInterpolate(xpos, ypos, missing, interpwins,par.edgeSampInterp);
+if q2Eyes
+    [llx ,lly ,llmiss]  = windowedInterpolate(data.left.X  ,data.left.Y  ,llmiss ,llinterpwins,par.edgeSampInterp);
+    [rrx ,rry ,rrmiss]  = windowedInterpolate(data.right.X ,data.right.Y ,rrmiss ,rrinterpwins,par.edgeSampInterp);
+end
+
+
+if ~q2Eyes
+    %% CALCULATE 2-MEANS CLUSTERING FOR SINGLE EYE
+    
+    % get kmeans-clustering for averaged signal
+    fprintf('2-Means clustering started for averaged signal \n');
+    [data.finalweights,stopped] = twoClusterWeighting(xpos,ypos,missingn,par.downsamples,par.chebyOrder,par.windowtime,par.steptime,par.freq,par.maxerrors);
+    
+    % check whether clustering succeeded
+    if stopped
+        fprintf('Clustering stopped after exceeding max errors, continuing to next file \n');
+        return
+    end
+    
+    %% CALCULATE 2-MEANS CLUSTERING FOR SEPARATE EYES
+elseif q2Eyes
+    % get kmeans-clustering for left eye signal
+    fprintf('2-Means clustering started for left eye signal \n');
+    [finalweights_left,stopped] = twoClusterWeighting(llx,lly,llmiss,par.downsamples,par.chebyOrder,par.windowtime,par.steptime,par.freq,par.maxerrors);
+    
+    % check whether clustering succeeded
+    if stopped
+        fprintf('Clustering stopped after exceeding max errors, continuing to next file \n');
+        return
+    end
+    
+    % get kmeans-clustering for right eye signal
+    fprintf('2-Means clustering started for right eye signal \n');
+    [finalweights_right,stopped] = twoClusterWeighting(rrx,rry,rrmiss,par.downsamples,par.chebyOrder,par.windowtime,par.steptime,par.freq,par.maxerrors);
+    
+    % check whether clustering succeeded
+    if stopped
+        fprintf('Clustering stopped after exceeding max errors, continuing to next file \n');
+        return
+    end
+    
+    %% AVERAGE FINALWEIGHTS OVER COMBINED & SEPARATE EYES
+    data.finalweights = nanmean([finalweights_left finalweights_right],2);
+end
+
+%% DETERMINE FIXATIONS BASED ON FINALWEIGHTS_AVG
+fprintf('Determining fixations based on clustering weight mean for averaged signal and separate eyes + 2*std \n')
+[fix.cutoff,fix.start,fix.end,fix.startT,fix.endT,fix.dur,fix.xpos,fix.ypos,fix.flankdataloss,fix.fracinterped] = getFixations(data.finalweights,data.time,xpos,ypos,missing,par.cutoffstd,par.maxMergeDist,par.maxMergeTime,par.minFixDur);
+[fix.RMSxy,fix.BCEA,fix.fixRangeX,fix.fixRangeY] = getFixStats(xpos,ypos,missing,fix.start,fix.end,pixperdeg);
+        
