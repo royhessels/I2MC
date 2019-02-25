c96c980feaf370c3f4a52d4b0218fd46668c7a3b
royhessels <royhessels@gmail.com>
2019-02-25 14:12:46 +0100

    Update I2MC.m

--- I2MC.m
+++ I2MC.m
@@ -5,10 +5,13 @@
 % occur.
 % 
 % Cite as:
-% Hessels, R.S., Niehorster, D.C., Kemner, C., & Hooge, I.T.C., (2016).
+% Hessels, R.S., Niehorster, D.C., Kemner, C., & Hooge, I.T.C., (2017).
 % Noise-robust fixation detection in eye-movement data - Identification by 
-% 2-means clustering (I2MC). Behavior Research Methods.
+% 2-means clustering (I2MC). Behavior Research Methods, 49(5):1802–1823.
 % 
+% Version:
+% v2.0.3
+%
 % For more information, questions, or to check whether we have updated to a
 % better version, e-mail: royhessels@gmail.com / dcnieho@gmail.com. I2MC is
 % available from www.github.com/royhessels/I2MC
