3f989b5cac859b59b2b557c83d8b4fbbc3cc8276
Diederick Niehorster <dcnieho@gmail.com>
2024-05-31 16:02:15 +0200

    importTobiiTX300: prevent association of v with velocity. its Tobii's validity codes

--- functions/import/importTobiiTX300.m
+++ functions/import/importTobiiTX300.m
@@ -7,18 +7,18 @@ dat = readintfile(file,nskip,28);
 timestamp   = dat(:,28);
 lx          = dat(:,8 ) * res(1);
 ly          = dat(:,9 ) * res(2);
-lv          = dat(:,14);
+lval        = dat(:,14);
 rx          = dat(:,21) * res(1);
 ry          = dat(:,22) * res(2);
-rv          = dat(:,27);
+rval        = dat(:,27);
 
 % sometimes we have weird peaks where one sample is (very) far outside the
 % monitor. Here, count as missing any data that is more than one monitor
 % distance outside the monitor.
-qMiss = lx<-res(1) | lx>2*res(1) | ly<-res(2) | ly>2*res(2) | lv>1;
+qMiss = lx<-res(1) | lx>2*res(1) | ly<-res(2) | ly>2*res(2) | lval>1;
 lx(qMiss) = missingx;
 ly(qMiss) = missingy;
-qMiss = rx<-res(1) | rx>2*res(1) | ry<-res(2) | ry>2*res(2) | rv>1;
+qMiss = rx<-res(1) | rx>2*res(1) | ry<-res(2) | ry>2*res(2) | rval>1;
 rx(qMiss) = missingx;
 ry(qMiss) = missingy;
 
